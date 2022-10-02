7132e02fb7452c08914ebe9b73d552583217f625
Zuhropenchik <zahvinar358@gmail.com>
2022-10-02 23:49:02 +0300

    cors integration + fixing error handling

--- internal/network/middleware/session.go
+++ internal/network/middleware/session.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"HeadHunter/internal/errorHandler"
 	"HeadHunter/internal/network/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -8,17 +9,17 @@ import (
 func Session(c *gin.Context) {
 	sessionToken, err := c.Cookie("session")
 	if err != nil {
-		_ = c.Error(ErrUnauthorized)
+		_ = c.Error(errorHandler.ErrUnauthorized)
 		return
 	}
 
 	userSession, err := sessions.SessionsStore.GetSession(sessions.Token(sessionToken))
 	if err != nil {
-		_ = c.Error(ErrUnauthorized)
+		_ = c.Error(errorHandler.ErrUnauthorized)
 		return
 	}
 	if userSession.IsExpired() {
-		_ = c.Error(ErrUnauthorized)
+		_ = c.Error(errorHandler.ErrUnauthorized)
 		return
 	}
 	c.Set("cookie", "verification value")
--- internal/network/router.go
+++ internal/network/router.go
@@ -3,18 +3,13 @@ package network
 import (
 	"HeadHunter/internal/network/handlers"
 	"HeadHunter/internal/network/middleware"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes() *gin.Engine {
 	router := gin.Default()
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:8000"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
-	config.AllowCredentials = true
 
-	router.Use(cors.New(config))
+	router.Use(middleware.CORSMiddleware())
 
 	auth := router.Group("/auth")
 	{
