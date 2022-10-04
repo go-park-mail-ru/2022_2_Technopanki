1330d5e48510bee330849d80c8675764ecda9a22
Zuhropenchik <zahvinar358@gmail.com>
2022-10-05 02:17:54 +0300

    more testcases for auth

--- internal/network/handlers/auth_test.go
+++ internal/network/handlers/auth_test.go
@@ -32,6 +32,42 @@ func Test_SignUp(t *testing.T) {
 			expectedResponseBody: "",
 		},
 		{
+			name: "Invalid email 1",
+			inputBody: `{
+    			"email": "example2mail.ru",
+    			"name": "Zahar",
+    			"surname": "Urvancev",
+    			"password": "111abc!!",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"email должен содержать @\"}",
+		},
+		{
+			name: "Invalid email 2",
+			inputBody: `{
+    			"email": "ex@",
+    			"name": "Zahar",
+    			"surname": "Urvancev",
+    			"password": "111abc!!",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"длина email должна быть между 8 and 30 символами\"}",
+		},
+		{
+			name: "user exists",
+			inputBody: `{
+    			"email": "example@mail.ru",
+    			"name": "Zakhar",
+    			"surname": "Urvancev",
+    			"password": "123456!!a",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"пользователь с таким email уже существует\"}",
+		},
+		{
 			name: "Invalid name",
 			inputBody: `{
     			"email": "invalid_example@mail.ru",
@@ -78,6 +114,30 @@ func Test_SignUp(t *testing.T) {
 			}`,
 			expectedStatusCode:   400,
 			expectedResponseBody: "{\"error\":\"пароль должен содержать буквы латиницы, цифры и спецсимволы(!#%^$)\"}",
+		},
+		{
+			name: "Invalid role",
+			inputBody: `{
+    			"email": "example2@mail.ru",
+    			"name": "Zakhar",
+    			"surname": "Urvancev",
+    			"password": "123456!!a",
+    			"role": "invalid"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"invalid input user role\"}",
+		},
+		{
+			name: "Invalid json",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"name": "Zahar",
+    			"surname": "Urvancev",
+    			"password": "111asdwwqv",
+    			"role": "applicant",
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "",
 		},
 	}
 	for _, testCase := range testTable {
@@ -131,6 +191,24 @@ func Test_SignIn(t *testing.T) {
 			}`,
 			expectedStatusCode:   401,
 			expectedResponseBody: "{\"error\":\"пользователя с таким email не существует\"}",
+		},
+		{
+			name: "Invalid password 1",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"password": "1a!"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"длина пароля должна быть между 8 и 20 символами\"}",
+		},
+		{
+			name: "Invalid password 2",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"password": "111asdwwqv"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"пароль должен содержать буквы латиницы, цифры и спецсимволы(!#%^$)\"}",
 		},
 		{
 			name: "invalid json",
