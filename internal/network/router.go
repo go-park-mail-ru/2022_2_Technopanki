2ee8f953d86ce6725943d41de68c1b3771cd0db8
Vladislav <sdvlad75@gmail.com>
2022-10-04 18:19:04 +0300

    logout fix

--- internal/network/router.go
+++ internal/network/router.go
@@ -14,7 +14,7 @@ func InitRoutes() *gin.Engine {
 	{
 		auth.POST("/sign-up", handlers.SignUp, middleware.ErrorHandler())
 		auth.POST("/sign-in", handlers.SignIn, middleware.ErrorHandler())
-		auth.POST("/logout", handlers.Logout, middleware.ErrorHandler())
+		auth.POST("/logout", handlers.Logout, middleware.Session, middleware.ErrorHandler())
 	}
 
 	api := router.Group("/api", middleware.Session)
