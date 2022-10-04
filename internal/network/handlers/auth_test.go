f8c17ade63b600e7d9a9d6a72250e95b3c6e3bc4
Vladislav <sdvlad75@gmail.com>
2022-10-04 18:19:10 +0300

    auth test

--- internal/network/handlers/auth_test.go
+++ internal/network/handlers/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"HeadHunter/internal/network/middleware"
 	"bytes"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
@@ -17,6 +18,7 @@ func Test_SignUp(t *testing.T) {
 		inputBody            string
 		expectedStatusCode   int
 		expectedResponseBody string
+		isValid              bool
 	}{
 		{
 			name: "Success case",
@@ -29,17 +31,60 @@ func Test_SignUp(t *testing.T) {
 			}`,
 			expectedStatusCode:   200,
 			expectedResponseBody: "",
+			isValid:              true,
 		},
-		//{
-		//	name: "Bad body case",
-		//	inputBody: `{
-		//		HEELLOO WORLD
-		//	}`,
-		//	expectedStatusCode: 400,
-		//	expectedResponseBody: `{
-		//		"error": "bad request"
-		//	}`,
-		//},
+		{
+			name: "Invalid name",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"name": "Z",
+    			"surname": "Urvancev",
+    			"password": "111abc!!",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"длина имени должна быть между 3 и 20 символами\"}",
+			isValid:              false,
+		},
+		{
+			name: "Invalid surname",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"name": "Zahar",
+    			"surname": "U",
+    			"password": "111abc!!",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"длина фамилии должна быть между 3 и 20 символами\"}",
+			isValid:              false,
+		},
+		{
+			name: "Invalid password 1",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"name": "Zahar",
+    			"surname": "Urvancev",
+    			"password": "111",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"длина пароля должна быть между 8 и 20 символами\"}",
+			isValid:              false,
+		},
+		{
+			name: "Invalid password 2",
+			inputBody: `{
+    			"email": "invalid_example@mail.ru",
+    			"name": "Zahar",
+    			"surname": "Urvancev",
+    			"password": "111asdwwqv",
+    			"role": "applicant"
+			}`,
+			expectedStatusCode:   400,
+			expectedResponseBody: "{\"error\":\"пароль должен содержать буквы латиницы, цифры и спецсимволы(!#%^$)\"}",
+			isValid:              false,
+		},
 	}
 	for _, testCase := range testTable {
 		tc := testCase
@@ -47,16 +92,21 @@ func Test_SignUp(t *testing.T) {
 			t.Parallel()
 			router := gin.New()
 			router.POST("/auth/sign-up", SignUp, middleware.ErrorHandler())
-			router.Use(middleware.ErrorHandler())
 			recorder := httptest.NewRecorder()
 
-			http.SetCookie(recorder, &http.Cookie{Name: "session", Value: "test_value"})
 			req, reqErr := http.NewRequest("POST", "/auth/sign-up",
 				bytes.NewBufferString(tc.inputBody))
 			require.NoError(t, reqErr)
 
 			router.ServeHTTP(recorder, req)
-			assert.Equal(t, "test_value", recorder.Result().Cookies()[0].Value)
+
+			if tc.isValid {
+				_, sessionTokenErr := uuid.Parse(recorder.Result().Cookies()[0].Value)
+				assert.NoError(t, sessionTokenErr)
+			} else {
+				assert.Equal(t, []*http.Cookie{}, recorder.Result().Cookies())
+			}
+
 			assert.Equal(t, tc.expectedStatusCode, recorder.Code)
 			assert.Equal(t, tc.expectedResponseBody, recorder.Body.String())
 		})
