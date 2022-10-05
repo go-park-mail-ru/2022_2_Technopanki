0144080e8a6ad4f4d503d5cdbbb760bfde41ad71
Zuhropenchik <zahvinar358@gmail.com>
2022-10-05 04:21:53 +0300

    fix vacancy handler + fix vacancy entity

--- internal/entity/vacancy.go
+++ internal/entity/vacancy.go
@@ -9,5 +9,7 @@ type Vacancy struct {
 	MinimalSalary int       `json:"minimal_salary"`
 	MaximumSalary int       `json:"maximum_salary"`
 	EmployerID    int       `json:"-"`
+	EmployerName  string    `json:"employer_name"`
+	City          string    `json:"city"`
 	Date          time.Time `json:"date"`
 }
