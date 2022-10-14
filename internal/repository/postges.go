da7c06f6641d65147d0fc0ea84bde5a4026fc518
Zuhropenchik <zahvinar358@gmail.com>
2022-10-15 02:14:47 +0300

    database fix(dont work)

--- internal/repository/postges.go
+++ internal/repository/postges.go
@@ -17,9 +17,11 @@ type Config struct {
 }
 
 func NewPostgresDB(cfg Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	//dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+	//	cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+		cfg.Username, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode)
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{})
 	if err != nil {
 		return &gorm.DB{}, err
 	}
