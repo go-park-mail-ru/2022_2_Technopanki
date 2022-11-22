a91ab0c2de77fcf260676b256767e77abeed11cd
Zuhropenchik <zahvinar358@gmail.com>
2022-11-23 01:21:26 +0300

    test for get resume by applicant + get resume preview(repo)

--- internal/repository/impl/resume_test.go
+++ internal/repository/impl/resume_test.go
@@ -1,6 +1,7 @@
 package impl
 
 import (
+	"HeadHunter/pkg/errorHandler"
 	"regexp"
 	"testing"
 
@@ -66,8 +67,6 @@ func TestResumePostgres_GetResume(t *testing.T) {
 		testCase := tc
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
-			// "is_current_job", "start_date", "end_date", "job_title", "company_name", "job_location_city",
-			//      "experience_description"
 			resumeRows := sqlmock.NewRows([]string{"id", "title", "description", "user_account_id"})
 			resumeRows = resumeRows.AddRow(testCase.expectedResume.ID, testCase.expectedResume.Title,
 				testCase.expectedResume.Description, testCase.expectedResume.UserAccountId)
@@ -88,12 +87,153 @@ func TestResumePostgres_GetResume(t *testing.T) {
 				WithArgs(1, 1).WillReturnRows(educationRows)
 
 			actualResume, getErr := ResumeDB.GetResume(1)
-			if getErr != nil {
-				t.Errorf("unexpected err: %s", getErr)
-				return
+
+			assert.Equal(t, testCase.expectedErr, getErr)
+			assert.Equal(t, testCase.expectedResume, actualResume)
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
+
+func TestResumePostgres_GetPreviewResumeByApplicant(t *testing.T) {
+	t.Parallel()
+	ResumeDB, mock, mockErr := CreateResumeMock()
+	if mockErr != nil {
+		t.Errorf("error with creating mock: %s", mockErr)
+	}
+
+	testTable := []struct {
+		name        string
+		id          uint
+		expectedErr error
+		expected    []*models.ResumePreview
+	}{
+		{
+			name:        "ok",
+			id:          1,
+			expectedErr: nil,
+			expected: []*models.ResumePreview{{
+				ApplicantName:    "Zakhar",
+				ApplicantSurname: "Urvancev",
+				Id:               1,
+				Title:            "Some title",
+			},
+			},
+		},
+		{
+			name:        "not found",
+			id:          5,
+			expectedErr: errorHandler.ErrResumeNotFound,
+			expected:    []*models.ResumePreview{},
+		},
+	}
+
+	for _, tc := range testTable {
+		testCase := tc
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			resumeRows := sqlmock.NewRows([]string{"id", "title", "applicant_name", "applicant_surname"})
+			if len(testCase.expected) > 0 {
+				resumeRows = resumeRows.AddRow(testCase.expected[0].Id, testCase.expected[0].Title,
+					testCase.expected[0].ApplicantName, testCase.expected[0].ApplicantSurname)
+			}
+			mock.
+				ExpectQuery(regexp.QuoteMeta(`SELECT user_accounts.applicant_name, user_accounts.applicant_surname, user_accounts.image,resumes.id, resumes.title, resumes.created_time FROM "resumes" left join user_accounts on resumes.user_account_id = user_accounts.id WHERE user_account_id = $1`)).
+				WithArgs(1).WillReturnRows(resumeRows)
+
+			actualResume, getErr := ResumeDB.GetPreviewResumeByApplicant(1)
+
+			assert.Equal(t, testCase.expectedErr, getErr)
+			assert.Equal(t, len(testCase.expected), len(actualResume))
+			for i := range actualResume {
+				assert.Equal(t, *testCase.expected[i], *actualResume[i])
 			}
 
-			assert.Equal(t, testCase.expectedResume, actualResume)
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
+
+func TestResumePostgres_GetResumeByApplicant(t *testing.T) {
+	t.Parallel()
+	ResumeDB, mock, mockErr := CreateResumeMock()
+	if mockErr != nil {
+		t.Errorf("error with creating mock: %s", mockErr)
+	}
+
+	testTable := []struct {
+		name        string
+		id          uint
+		expectedErr error
+		expected    []*models.Resume
+	}{
+		{
+			name:        "ok",
+			id:          1,
+			expectedErr: nil,
+			expected: []*models.Resume{{
+				ID:            1,
+				Title:         "title",
+				Description:   "desc",
+				UserAccountId: 1,
+				ExperienceDetail: models.ExperienceDetail{
+					ResumeId: 1,
+				},
+				EducationDetail: models.EducationDetail{
+					ResumeId: 1,
+				},
+			}},
+		},
+		{
+			name:        "not found",
+			id:          5,
+			expectedErr: errorHandler.ErrResumeNotFound,
+			expected:    []*models.Resume{},
+		},
+	}
+
+	for _, tc := range testTable {
+		testCase := tc
+		t.Run(testCase.name, func(t *testing.T) {
+
+			resumeRows := sqlmock.NewRows([]string{"id", "title", "description", "user_account_id"})
+			if len(testCase.expected) > 0 {
+				resumeRows = resumeRows.AddRow(testCase.expected[0].ID, testCase.expected[0].Title,
+					testCase.expected[0].Description, testCase.expected[0].UserAccountId)
+			}
+			mock.
+				ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "resumes" left join experience_details on resumes.id = experience_details.resume_id left join education_details on resumes.id = education_details.resume_id WHERE user_account_id = $1`)).
+				WithArgs(1).WillReturnRows(resumeRows)
+
+			experienceRows := sqlmock.NewRows([]string{"resume_id"})
+			if len(testCase.expected) > 0 {
+				experienceRows = experienceRows.AddRow(testCase.expected[0].ExperienceDetail.ResumeId)
+			}
+			mock.
+				ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "resumes" left join experience_details on resumes.id = experience_details.resume_id left join education_details on resumes.id = education_details.resume_id WHERE user_account_id = $1`)).
+				WithArgs(1).WillReturnRows(experienceRows)
+
+			educationRows := sqlmock.NewRows([]string{"resume_id"})
+			if len(testCase.expected) > 0 {
+				educationRows = educationRows.AddRow(testCase.expected[0].ExperienceDetail.ResumeId)
+			}
+			mock.
+				ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "resumes" left join experience_details on resumes.id = experience_details.resume_id left join education_details on resumes.id = education_details.resume_id WHERE user_account_id = $1`)).
+				WithArgs(1).WillReturnRows(educationRows)
+
+			actualResume, getErr := ResumeDB.GetResumeByApplicant(1)
+
+			assert.Equal(t, testCase.expectedErr, getErr)
+			assert.Equal(t, len(testCase.expected), len(actualResume))
+			for i := range actualResume {
+				assert.Equal(t, *testCase.expected[i], *actualResume[i])
+			}
+
 			if err := mock.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
 			}
