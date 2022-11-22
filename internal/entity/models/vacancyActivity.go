98484240c92e8896c2b209ae4a951ec6d719fed6
AkimEgorov <egorov.akim@mail.ru>
2022-11-22 20:02:35 +0300

    поменял id резюме

--- internal/entity/models/vacancyActivity.go
+++ internal/entity/models/vacancyActivity.go
@@ -4,7 +4,7 @@ import "time"
 
 type VacancyActivity struct {
 	UserAccountId uint      `json:"user_account_id" gorm:"primaryKey"`
-	ResumeId      uint      `json:"resume_id" gorm:"primaryKey"`
+	ResumeId      uint      `json:"id" gorm:"primaryKey"`
 	VacancyId     uint      `json:"vacancy_id" gorm:"primaryKey"`
 	UserName      string    `json:"user_name"`
 	UserSurname   string    `json:"user_surname"`
