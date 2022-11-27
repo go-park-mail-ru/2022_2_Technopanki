9bbab7b9f8792bc20493b50be0e3cc4f53375c36
Zuhropenchik <zahvinar358@gmail.com>
2022-11-28 01:21:57 +0300

    add auth microservice

---
+++ auth_microservice/handler/impl/session.go
@@ -0,0 +1,72 @@
+package impl
+
+import (
+	"HeadHunter/auth_microservice/handler"
+	"HeadHunter/auth_microservice/usecase/impl"
+	"HeadHunter/pkg/errorHandler"
+	"context"
+)
+
+type SessionHandler struct {
+	sessionUseCase impl.SessionUseCase
+	handler.UnimplementedAuthCheckerServer
+}
+
+func NewSessionHandler(_sus impl.SessionUseCase) *SessionHandler {
+	return &SessionHandler{sessionUseCase: _sus}
+}
+
+func (sh *SessionHandler) NewSession(ctx context.Context, in *handler.Email) (*handler.Token, error) {
+	if in == nil {
+		return &handler.Token{}, errorHandler.ErrBadRequest
+	}
+	token, createErr := sh.sessionUseCase.NewSession(in.Value)
+	if createErr != nil {
+		return &handler.Token{}, createErr
+	}
+	return &handler.Token{Value: token}, nil
+}
+
+func (sh *SessionHandler) GetSession(ctx context.Context, in *handler.Token) (*handler.Email, error) {
+	if in == nil {
+		return &handler.Email{}, errorHandler.ErrBadRequest
+	}
+	email, getErr := sh.sessionUseCase.GetSession(in.Value)
+	if getErr != nil {
+		return &handler.Email{}, getErr
+	}
+	return &handler.Email{Value: email}, nil
+}
+
+func (sh *SessionHandler) DeleteSession(ctx context.Context, in *handler.Token) (*handler.Nothing, error) {
+	if in == nil {
+		return &handler.Nothing{}, errorHandler.ErrBadRequest
+	}
+	deleteErr := sh.sessionUseCase.DeleteSession(in.Value)
+	if deleteErr != nil {
+		return &handler.Nothing{}, deleteErr
+	}
+	return &handler.Nothing{}, nil
+}
+
+func (sh *SessionHandler) CreateConfirmationCode(ctx context.Context, in *handler.Email) (*handler.Token, error) {
+	if in == nil {
+		return &handler.Token{}, errorHandler.ErrBadRequest
+	}
+	code, createErr := sh.sessionUseCase.CreateConfirmationCode(in.Value)
+	if createErr != nil {
+		return &handler.Token{}, createErr
+	}
+	return &handler.Token{Value: code}, nil
+}
+
+func (sh *SessionHandler) GetEmailFromCode(ctx context.Context, in *handler.Token) (*handler.Email, error) {
+	if in == nil {
+		return &handler.Email{}, errorHandler.ErrBadRequest
+	}
+	email, getErr := sh.sessionUseCase.GetEmailFromCode(in.Value)
+	if getErr != nil {
+		return &handler.Email{}, getErr
+	}
+	return &handler.Email{Value: email}, nil
+}
---
+++ auth_microservice/repository/repository.go
@@ -0,0 +1,11 @@
+package repository
+
+//go:generate mockgen -source repository.go -destination=mocks/mock.go
+
+type Repository interface {
+	NewSession(email string) (string, error)
+	GetSession(token string) (string, error)
+	DeleteSession(token string) error
+	CreateConfirmationCode(email string) (string, error)
+	GetEmailFromCode(token string) (string, error)
+}
