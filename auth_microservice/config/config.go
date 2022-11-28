43acd8772f4a5e76aa06eac93f26994b3dbc3964
Zuhropenchik <zahvinar358@gmail.com>
2022-11-29 00:00:27 +0300

    fix rebase conflict 7/12

---
+++ auth_microservice/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"HeadHunter/configs"
+	"gopkg.in/yaml.v2"
+	"os"
+	"path/filepath"
+)
+
+type SessionConfig struct {
+	DefaultExpiringSession int `yaml:"defaultExpiringSession"`
+	ConfirmationTime       int `yaml:"confirmationTime"`
+	Redis                  configs.RedisConfig
+}
+
+func InitConfig(config *SessionConfig) error {
+	filename, fileErr := filepath.Abs(".auth_microservice/configs/config.yml")
+	if fileErr != nil {
+		return fileErr
+	}
+
+	yamlFile, yamlErr := os.ReadFile(filename)
+	if yamlErr != nil {
+		return yamlErr
+	}
+
+	marshalErr := yaml.Unmarshal(yamlFile, config)
+	if marshalErr != nil {
+		return marshalErr
+	}
+
+	return nil
+}
