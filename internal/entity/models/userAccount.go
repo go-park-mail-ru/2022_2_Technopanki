64cb3cb450219e34eae34b6acfb73cb4e060aad9
Zuhropenchik <zahvinar358@gmail.com>
2022-12-01 14:28:40 +0300

    add business_type in safe user fields

--- internal/entity/models/userAccount.go
+++ internal/entity/models/userAccount.go
@@ -51,7 +51,7 @@ var PrivateUserFields = []string{"email", "contact_number",
 // SafeUserFields Поля, доступ к которым доступен всегда
 var SafeUserFields = []string{"id", "user_type", "description", "status", "date_of_birth", "image",
 	"applicant_name", "applicant_surname", "company_name",
-	"location", "company_size", "company_website_url", "public_fields", "is_confirmed"}
+	"location", "company_size", "company_website_url", "public_fields", "is_confirmed", "business_type"}
 
 const NoPublicFields string = "null"
 
