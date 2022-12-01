d6499f83b5480060180857a9c5a0eb0984ddce80
Zuhropenchik <zahvinar358@gmail.com>
2022-12-01 07:24:00 +0300

    fix applies(kostilno)

--- internal/repository/impl/vacancy_activity.go
+++ internal/repository/impl/vacancy_activity.go
@@ -14,11 +14,23 @@ func NewVacancyActivityPostgres(db *gorm.DB) *VacancyActivityPostgres {
 	return &VacancyActivityPostgres{db: db}
 }
 
-func (vap *VacancyActivityPostgres) GetAllVacancyApplies(vacancyId uint) ([]*models.VacancyActivity, error) {
+func (vap *VacancyActivityPostgres) GetAllVacancyApplies(vacancyId uint) ([]*models.VacancyActivity, error) { //TODO Переделать
 	var applies []*models.VacancyActivity
 	query := vap.db.Where("vacancy_id = ?", vacancyId).Find(&applies)
 	if query.Error != nil {
 		return applies, query.Error
+	}
+	var user models.UserAccount
+	if len(applies) > 0 {
+		queryUser := vap.db.Where("id = ?", applies[0].UserAccountId).Find(&user)
+		if queryUser.Error != nil {
+			return applies, queryUser.Error
+		}
+	}
+	for _, elem := range applies {
+		elem.Image = user.Image
+		elem.ApplicantName = user.ApplicantName
+		elem.ApplicantSurname = user.ApplicantSurname
 	}
 	return applies, nil
 }
@@ -41,11 +53,23 @@ func (vap *VacancyActivityPostgres) ApplyForVacancy(apply *models.VacancyActivit
 	return QueryValidation(query, "vacancy_activity")
 }
 
-func (vap *VacancyActivityPostgres) GetAllUserApplies(userId uint) ([]*models.VacancyActivity, error) {
+func (vap *VacancyActivityPostgres) GetAllUserApplies(userId uint) ([]*models.VacancyActivity, error) { //TODO переделать
 	var applies []*models.VacancyActivity
+	var user models.UserAccount
+	queryUser := vap.db.Where("id = ?", userId).Find(&user)
+	if queryUser.Error != nil {
+		return applies, queryUser.Error
+	}
+
 	query := vap.db.Where("user_account_id = ?", userId).Find(&applies)
 	if query.Error != nil {
 		return applies, query.Error
+	}
+
+	for _, elem := range applies {
+		elem.Image = user.Image
+		elem.ApplicantName = user.ApplicantName
+		elem.ApplicantSurname = user.ApplicantSurname
 	}
 	return applies, nil
 }
