3184e638bf51a0fe3e23ebb25fccb9f32e10765f
Zuhropenchik <zahvinar358@gmail.com>
2022-12-01 03:11:30 +0300

    fix images

--- internal/usecases/impl/user.go
+++ internal/usecases/impl/user.go
@@ -266,7 +266,7 @@ func (us *UserService) UploadUserImage(user *models.UserAccount, fileHeader *mul
 	}
 
 	user = escaping.EscapingObject[*models.UserAccount](user)
-	imageName := fmt.Sprintf("%d.webp?", user.ID)
+	imageName := fmt.Sprintf("%d.webp", user.ID)
 	user.Image = fmt.Sprintf("%d.webp?%d", user.ID, time.Now().Unix())
 
 	updateErr := us.userRep.UpdateUser(&models.UserAccount{ID: user.ID, Image: user.Image})
