280e4e8b53b916ff4e0df838c83dc499b2ded947
Zuhropenchik <zahvinar358@gmail.com>
2022-12-15 19:42:26 +0300

    cleaning code

--- internal/entity/models/notification.go
+++ internal/entity/models/notification.go
@@ -11,13 +11,13 @@ type Notification struct {
 var AllowedNotificationTypes = []string{"apply", "download resume"}
 
 type NotificationPreview struct {
-	ID            uint   `json:"id"`             //**
-	Type          string `json:"type"`           //**
-	UserToID      uint   `json:"user_to_id"`     //**
-	UserFromID    uint   `json:"user_from_id"`   //**
-	ObjectId      uint   `json:"object_id"`      //id вакансии либо резюме
-	ApplicantName string `json:"applicant_name"` //applicant
-	Title         string `json:"title"`          //vacancy
+	ID            uint   `json:"id"`
+	Type          string `json:"type"`
+	UserToID      uint   `json:"user_to_id"`
+	UserFromID    uint   `json:"user_from_id"`
+	ObjectId      uint   `json:"object_id"` //id вакансии либо резюме
+	ApplicantName string `json:"applicant_name"`
+	Title         string `json:"title"`
 
-	CompanyName string `json:"company_name"` //company
+	CompanyName string `json:"company_name"`
 }
