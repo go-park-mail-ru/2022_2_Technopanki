5f1bb41791a6dc859c25474222ca97ea7705a012
Zuhropenchik <zahvinar358@gmail.com>
2022-12-21 20:57:33 +0300

    people cant send notification to himself

--- internal/usecases/impl/notification.go
+++ internal/usecases/impl/notification.go
@@ -38,6 +38,11 @@ func (ns *NotificationService) CreateNotification(notification *models.Notificat
 	if _, ok := models.AllowedNotificationTypes[notification.Type]; !ok {
 		return nil, errorHandler.ErrBadRequest
 	}
+
+	if notification.UserFromID == notification.UserToID {
+		return &models.NotificationPreview{UserToID: notification.UserToID, UserFromID: notification.UserFromID}, nil
+	}
+
 	createErr := ns.notificationRepo.CreateNotification(notification)
 	if createErr != nil {
 		return nil, createErr
