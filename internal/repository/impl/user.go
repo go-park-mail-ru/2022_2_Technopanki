726d8f90eee8194eb28e0649f188659b56e7bd99
Zuhropenchik <zahvinar358@gmail.com>
2022-12-21 15:44:38 +0300

    delete useless func, few fixes

--- internal/repository/impl/user.go
+++ internal/repository/impl/user.go
@@ -2,7 +2,6 @@ package impl
 
 import (
 	"HeadHunter/internal/entity/models"
-	"fmt"
 	"gorm.io/gorm"
 	"strings"
 )
@@ -43,7 +42,7 @@ func (up *UserPostgres) GetUser(id uint) (*models.UserAccount, error) {
 func (up *UserPostgres) GetAllUsers(conditions []string, filterValues []interface{}, flag string) ([]*models.UserAccount, error) {
 	var users []*models.UserAccount
 	if conditions == nil {
-		query := up.db.Where("user_type = ?", flag).Find(&users)
+		query := up.db.Select(models.SafeUserFields).Where("user_type = ?", flag).Find(&users)
 		if query.Error != nil {
 			return users, query.Error
 		}
@@ -65,23 +64,4 @@ func (up *UserPostgres) GetUserSafety(id uint, allowedFields []string) (*models.
 
 	query := up.db.Select(append(models.SafeUserFields, allowedFields...)).Find(&result, id)
 	return &result, QueryValidation(query, "user")
-}
-
-func (up *UserPostgres) GetBestVacanciesForApplicant() ([]*models.Vacancy, error) {
-	var result []*models.Vacancy
-	query := up.db.Table("vacancies").Order("created_date asc").Limit(10).Scan(&result)
-	if query.Error != nil {
-		return nil, fmt.Errorf("error with getting best vacancies: %w", query.Error)
-	}
-	return result, nil
-}
-
-func (up *UserPostgres) GetBestApplicantForEmployer() ([]*models.UserAccount, error) {
-	var result []*models.UserAccount
-	query := up.db.Table("user_accounts").Select(models.SafeUserFields).Where("user_accounts.user_type = ?", "applicant").
-		Order("created_time asc").Limit(10).Scan(&result)
-	if query.Error != nil {
-		return nil, fmt.Errorf("error with getting best applicants: %w", query.Error)
-	}
-	return result, nil
 }
